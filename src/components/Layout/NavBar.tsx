8a62de0945dd1d4ebaee6e239d7462c5fbd8bcc4
Evandro Lima <96010876+EvandroLimaBarbosa@users.noreply.github.com>
2022-11-13 13:11:05 -0300

    add shadow

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,7 +1,7 @@
 export default function 
 () {
   return (
-    <div className="mx-auto pr-6 flex flex-row items-center bg-[#20345F] text-white justify-between">
+    <div className="mx-auto pr-6 flex flex-row items-center bg-[#20345F] text-white justify-between shadow-[0_0px_5px_4px_#0000008a]">
     <div className="bg-[#172645] pl-6 h-[70px] w-[280px] flex items-center">das</div>
     <div>sda</div>
   </div>
