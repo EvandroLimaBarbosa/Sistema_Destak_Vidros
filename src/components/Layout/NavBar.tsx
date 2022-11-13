de0de1f4d91c0693acec9c596a1ba42c2e43f1c1
Evandro Lima <96010876+EvandroLimaBarbosa@users.noreply.github.com>
2022-11-13 11:01:00 -0300

    Att

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-
 export default function 
 () {
   return (
-    <div className="mx-auto px-6 flex flex-row items-center bg-[#20345F] text-white justify-between">
-    <div className="bg-[#172645] h-[70px] w-[280px] flex items-center">das</div>
+    <div className="mx-auto pr-6 flex flex-row items-center bg-[#20345F] text-white justify-between">
+    <div className="bg-[#172645] pl-6 h-[70px] w-[280px] flex items-center">das</div>
     <div>sda</div>
   </div>
   )
