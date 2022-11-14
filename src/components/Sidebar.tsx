9f777024afeab98d42b5ef79e3e9167025ad94a9
Evandro Lima <96010876+EvandroLimaBarbosa@users.noreply.github.com>
2022-11-14 09:07:00 -0300

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 export default function Sidebar() {
   return (
-    <div className="bg-[#172645] top-[70px]  h-[500px] left-0 w-[280px] absolute  animate-spin">
+    <div className="bg-[#172645] top-[70px]  h-[500px] left-0 w-[280px] absolute">
       <a href="">Orçamentos</a>
     </div>
   );
