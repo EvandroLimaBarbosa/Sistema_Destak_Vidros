03916966eeaf37031ca340694bfc480e30c18c72
Evandro Lima <96010876+EvandroLimaBarbosa@users.noreply.github.com>
2022-11-14 09:00:44 -0300

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 export default function Sidebar() {
   return (
-    <div className="bg-[#172645] top-[70px] left-0 h-[100vh] w-[280px] absolute">
+    <div className="bg-[#172645] top-[70px] left-0 w-[280px] absolute">
       <a href="">Orçamentos</a>
     </div>
   );
