f41bb7dadbf91869842f58171c8ad797f5a8c719
Evandro Lima <96010876+EvandroLimaBarbosa@users.noreply.github.com>
2022-11-16 08:22:50 -0300

    Update ItemsSidebar.tsx

--- src/components/ItemsSidebar.tsx
+++ src/components/ItemsSidebar.tsx
@@ -5,7 +5,7 @@ interface ItemsSidebarProps {
 
 export default function ItemsSidebar(props : ItemsSidebarProps) {
   return (
-    <a href={`/${props.link}`} className="h-[40px] hover:bg-[#20345F] flex hover:text-gray-400 text-lg pl-10 items-center">
+    <a href={`/${props.link}`} className="h-[50px] hover:bg-[#20345F] flex hover:text-gray-400 text-lg pl-10 items-center">
       {props.value}
     </a>
   );
