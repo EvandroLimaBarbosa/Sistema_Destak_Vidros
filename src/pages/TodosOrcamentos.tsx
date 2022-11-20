3e69cff76acf232b19d04280bbf38d6c6c84ccc7
Evandro Lima <96010876+EvandroLimaBarbosa@users.noreply.github.com>
2022-11-19 22:48:15 -0300

    Adicionando bordas

--- src/pages/TodosOrcamentos.tsx
+++ src/pages/TodosOrcamentos.tsx
@@ -1,6 +1,11 @@
 
 export default function TodosOrcamentos() {
   return (
-    <div>TodosOrcamentos</div>
+      // container principal
+    <div
+      className={`flex rounded-sm border-[2px] border-[#A4A4A4] m-[15px] h-[650px]`}
+    >
+
+</div>
   )
 }
