28ef398137337f5759693cc59bffbc66f6af51d6
Evandro Lima <96010876+EvandroLimaBarbosa@users.noreply.github.com>
2022-11-21 10:10:30 -0300

    Update Header.tsx

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -26,13 +26,13 @@ export default function Header() {
     //  Conteiner de todo o menu
     <div className="mx-auto h-[70px] pr-6 flex flex-row items-center bg-[#20345F] text-white justify-between shadow-[0_5px_10px_4px_#0000008a]">
       {/* lado esquerdo do menu */}
-      <div>
+     
         <Sidebar
           icon={sidebar ? CloseIcon : HamburgIcon}
           activeAllScreen={sidebar}
           onClick={showSidebar}
         />
-      </div>
+      
 
       {/* lado direito do menu */}
       <div>Sair</div>
